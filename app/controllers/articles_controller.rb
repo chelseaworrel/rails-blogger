d9841bb1863ae45602a0f409a878437aaab81875
Chelsea Worrel <chelseaworrel@gmail.com>
2015-06-03 15:33:56 -0600

    form partial for both edit and create made

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,51 @@
 class ArticlesController < ApplicationController
+
+  before_action(:set_article, only: [:show, :edit, :update, :destroy])
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      flash[:errors] = "Please try again!"
+      render(:new)
+    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to(articles_path)
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  private
+
+  def article_params #use this when you get a ActiveModel::ForbiddenAttributesError
+    params.require(:article).permit(:title, :body)
+  end
+
+  def set_article
+    @article = Article.find(params[:id]) #params[:id] comes from the url
   end
 end
+
+# C -create method
+# R -index method
+# U - need edit + update method
+# D - destroy method
